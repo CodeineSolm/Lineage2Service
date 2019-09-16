96c30f0610fdccbcec5d924ce892ba8f9cdd0411
Max <bloodmailvol1@gmail.com>
2019-09-16 20:53:08 +0300

    only 1 page

---
+++ js/script.js
@@ -0,0 +1,25 @@
+function SetImg(selected_url)
+{
+    var item = document.getElementById('preview');
+    item.src = selected_url;
+}
+
+function SubmitForm(name,tel,car)
+{
+    $.ajax({
+        method:"POST", url:"/Home/Order",
+        data:{Name:name,Tel:tel,Services:services}
+    }).fail(function(){
+        alert("При передаче данных произошла ошибка!");
+    }).done(function(context){
+        alert(context)
+    });
+}
+
+$(document).ready(function()
+{
+    $('#header').fadeTo(700,0.7,function()
+    {
+        $('html, body').animate({scrollTop: $('body').height()}, 700);
+    });
+});
